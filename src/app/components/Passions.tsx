c74c59da8d8dc30f98b69e76117bd9d075e63299
Juan Lagunas <juanlagunas28@gmail.com>
2024-08-22 00:31:46 -0400

    add passions text

--- src/app/components/Passions.tsx
+++ src/app/components/Passions.tsx
@@ -1,19 +1,27 @@
 export default function Passions() {
     return (
         <div className="text-sm">
-            <div className="text-base font-bold mb-2">Passions</div>
             <div className="grid grid-cols-2 gap-6">
                 <div className="col-span-2 md:col-span-1 h-96 w-full max-w-[600px] mx-auto bg-[#0D0D0D] border-e-2 border-[#00000] p-4">
-                    <h1 className="font-bold text-xl mb-2">Acquiring knowledge</h1>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed eleifend urna. Aliquam erat volutpat. Suspendisse eget tellus vitae lorem mattis luctus. Nulla sit amet placerat magna. Aenean lobortis auctor purus eget molestie. Vestibulum pretium massa nec risus dapibus, eu mollis odio accumsan. Integer augue nunc, ornare at justo vel, efficitur iaculis massa. In ut porttitor lorem. Suspendisse iaculis dui et sem facilisis tincidunt.
+                    <h1 className="font-bold text-xl mb-2">From Gamer to Lifelong Learner: My Unexpected Journey</h1>
+                    In high school, my priorities were pretty clear: study just enough to keep the grades up, and then dive back into the latest video game. But over the past year, something shifted. I found myself drawn to learning in a way I never expected.
+                    <br></br>
+                    <br></br>
+                    It started with Harvard&apos;s CS50 course, which was so engaging that I wanted more. That led me to explore MIT&apos;s OpenCourseWare, where I&apos;ve been diving into subjects I never imagined I&apos;d enjoy. It&apos;s been a wild ride, and I&apos;m excited to see where this newfound passion for learning takes me next!
                 </div>
                 <div className="col-span-2 md:col-span-1 h-96 w-full max-w-[600px] mx-auto bg-[#0D0D0D] border-e-2 border-[#00000] p-4">
-                    <h1 className="font-bold text-xl mb-2">Sharing it with others</h1>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed eleifend urna. Aliquam erat volutpat. Suspendisse eget tellus vitae lorem mattis luctus. Nulla sit amet placerat magna. Aenean lobortis auctor purus eget molestie. Vestibulum pretium massa nec risus dapibus, eu mollis odio accumsan. Integer augue nunc, ornare at justo vel, efficitur iaculis massa. In ut porttitor lorem. Suspendisse iaculis dui et sem facilisis tincidunt.
+                    <h1 className="font-bold text-xl mb-2">My Journey from Student to Teacher: Finding Purpose in Helping Others</h1>
+                        Growing up in an immigrant family, I didn&apos;t have much academic support. Schoolwork often felt like a solo mission, and I had to figure things out on my own. It wasn&apos;t always easy, and that experience shaped my perspective on education.
+
+                        Recently, I had the chance to help my brother with his high school programming class. What started as a way to lend a hand turned into something more meaningful. I discovered a passion for teaching. Seeing him grasp difficult concepts and succeed was incredibly rewarding, and it made me realize how much I enjoy helping others learn.
+
+                        This experience opened my eyes to the impact that a little support can have. I want to do more of this—not just for my brother, but for others who might be facing the same challenges I did growing up. I want to offer the help that I didn&apos;t have.
+
+                        So, if you&apos;re struggling with school or need some guidance, feel free to reach out.                      
                 </div>
                 <div className="col-span-2 h-96 w-full bg-[#0D0D0D] border-e-2 border-[#00000] p-4">
                     <h1 className="font-bold text-xl mb-2">Spending time with family</h1>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed eleifend urna. Aliquam erat volutpat. Suspendisse eget tellus vitae lorem mattis luctus. Nulla sit amet placerat magna. Aenean lobortis auctor purus eget molestie. Vestibulum pretium massa nec risus dapibus, eu mollis odio accumsan. Integer augue nunc, ornare at justo vel, efficitur iaculis massa. In ut porttitor lorem. Suspendisse iaculis dui et sem facilisis tincidunt.
+                                  
                 </div>
             </div>
         </div>
