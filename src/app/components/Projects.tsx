8de1a42d706f95fcb1964de151eabc6f205dd8da
Juan Lagunas <juanlagunas28@gmail.com>
2024-08-22 12:11:53 -0400

    remove projects section for now

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,15 +1,6 @@
 export default function Projects() {
     return (
-        <section className="w-full h-fit overflow-hidden">
-            <div className="bg-white grid grid-flow-col gap-4">
-                <div className="h-40 aspect-video bg-red-200"></div>
-                <div className="h-40 aspect-video bg-red-200"></div>
-                <div className="h-40 aspect-video bg-red-200"></div>
-                <div className="h-40 aspect-video bg-red-200"></div>
-                <div className="h-40 aspect-video bg-red-200"></div>
-                <div className="h-40 aspect-video bg-red-200"></div>
-                <div className="h-40 aspect-video bg-red-200"></div>
-            </div>
+        <section className="">
         </section>
     )
 }
