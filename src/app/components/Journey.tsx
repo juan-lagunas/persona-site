cef98269e3c142a47bd04fa2633d71e3aaacddee
Juan Lagunas <juanlagunas28@gmail.com>
2024-08-25 18:33:37 -0400

    change text color

--- src/app/components/Journey.tsx
+++ src/app/components/Journey.tsx
@@ -3,14 +3,14 @@ export default function Journey() {
         <section id="journey" className="text-base lg:text-lg h-fit max-w-[1450px] mx-auto">
             <div className="grid grid-cols-2 gap-10 h-fit">
                 <div className="col-span-2 lg:col-span-1 w-full max-w-[600px] mx-auto border-[#00000] md:p-4">
-                    <h1 className="font-bold text-2xl md:text-3xl mb-2 text-[#B3A369]">From Gamer to Lifelong Learner</h1>
+                    <h1 className="font-bold text-2xl md:text-3xl mb-2">From Gamer to Lifelong Learner</h1>
                     In high school, my priorities were simple: study just enough to keep the grades up, and then dive back into video games whenever I wasn&apos;t working. But something changed this past year. I stumbled upon Harvard&apos;s CS50 course, and to my surprise, I got hooked.
                     <br />
                     <br />
                     Since then, I&apos;ve explored even more through MIT&apos;s OpenCourseWare. It&apos;s been an unexpected journey, but a rewarding one. I&apos;m excited to keep leveling up my knowledge and see where this new passion takes me!
                 </div>
                 <div className="col-span-2 lg:col-span-1 w-full max-w-[600px] mx-auto border-[#00000] md:p-4">
-                    <h1 className="font-bold text-2xl md:text-3xl mb-2 text-[#B3A369]">Finding Purpose in Helping Others</h1>
+                    <h1 className="font-bold text-2xl md:text-3xl mb-2">Finding Purpose in Helping Others</h1>
                         Growing up in an immigrant family, I often found myself navigating school and planning for the future without much guidance. I didn&apos;t have someone to help me understand my academic options or prepare me for life after high school.
                         <br />
                         <br />
