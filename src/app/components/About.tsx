9e2b7cc82faac3cf36caf34d4259bf6c0d563449
Juan Lagunas <juanlagunas28@gmail.com>
2024-08-25 18:31:09 -0400

    change text color

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -10,7 +10,7 @@ export default function About() {
             <p className="text-sm font-thin italic text-[#cfcfcf] mb-2">A picture of my fiancé and I during the 2024 total solar eclipse in Carbondale, Illinois.</p>
             <h1 className="text-5xl lg:text-7xl font-bold mb-2">Hello, World!</h1>
             <div className="md:text-lg lg:text-2xl ">
-                My name is <a href="#" className="font-bold text-[#D99748] hover:scale-105">Juan Alberto Lagunas-Lara</a>, but to my family, I&apos;m known as Betillo or Tillo (&quot;Tee-yo&quot;).
+                My name is <a href="#" className="font-bold text-[#B3A369] hover:scale-105">Juan Alberto Lagunas-Lara</a>, but to my family, I&apos;m known as Betillo or Tillo (&quot;Tee-yo&quot;).
                 I&apos;m an enthusiastic electrical engineering student who&apos;s deeply into robotics and passionate about empowering my Mexican community. I&apos;ve always been curious about how things work, and I love learning as much as I enjoy sharing what I&apos;ve picked up along the way.
 
                 If you&apos;re here to connect, collaborate, or just see what I&apos;m up to, then you&apos;re in the right place.
--- src/app/components/SideNav.tsx
+++ src/app/components/SideNav.tsx
@@ -17,7 +17,7 @@ const SideNav: React.FC<SideNavProps> = ({ children }) => {
     }
     
     return (
-        <div className="flex flex-col h-dvh">
+        <div className="flex flex-col h-dvh text-[#B3A369]">
             <div className="flex overflow-auto h-full">
                 <div className="grid grid-rows-3 w-[20%] max-w-[200px] h-full py-10 bg-[#0D0D0D]">
                     <div className="text-center text-3xl font-bold">Tillo</div>
@@ -26,13 +26,10 @@ const SideNav: React.FC<SideNavProps> = ({ children }) => {
                         <div>Projects</div>
                         <div>Contact</div>
                     </div>
-                    <div className="flex flex-col items-center justify-end gap-y-4 text-white">
+                    <div className="flex flex-col items-center justify-end gap-y-4">
                         <a href="https://github.com/juan-lagunas" className="w-8 h-8 rounded-full flex items-center justify-center"><FontAwesomeIcon className="size-6" icon={faGithub} /></a>
                         <a href="https://www.youtube.com/@betillo-lagunas" className="w-8 h-8 rounded-full flex items-center justify-center"><FontAwesomeIcon className="size-6" icon={faYoutube} /></a>
                         <a href="mailto:juanlagunas28@gmail.com" className="w-8 h-8 rounded-full flex items-center justify-center"><FontAwesomeIcon className="size-6" icon={faEnvelope} /></a>
-                        <a href="#" onClick={toggleLanguage} className="text-white font-thin">
-                            {inSpanish ? "English" : "Español"}
-                        </a>
                     </div>
                 </div>
                 <main className="flex-1 overflow-y-auto bg-[#121212]">{children}</main>
