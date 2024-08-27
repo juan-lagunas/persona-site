9b0053c2980083e44f9b8d88c40933daf7c22aba
Juan Lagunas <juanlagunas28@gmail.com>
2024-08-27 16:48:04 -0400

    update

--- src/app/components/Empower.tsx
+++ src/app/components/Empower.tsx
@@ -1,12 +1,12 @@
 export default function Support() {
     return (
         <section id="empower" className="h-fit max-w-[1450px] mx-auto">
-            <h1 className="font-bold text-3xl md:text-5xl mb-2">Empower Our Community: Turning Passion into Action</h1>
+            <h1 className="font-bold text-3xl md:text-5xl mb-2">Empowering Others: Turning Passion into Action</h1>
             <p>
-                Now that I&apos;ve found my purpose, I&apos;m dedicated to uplifting our Mexican community by helping students who face the same challenges I once did. I believe that education is the key to opening doors, and I&apos;m here to make sure those doors are wide open for anyone who wants to walk through them.
+                Now that I&apos;ve found my purpose, I&apos;m committed to helping students who face challenges like I did. I believe that education is the key to opening doors, and I&apos;m here to make sure those doors are wide open for anyone ready to walk through them.
                 <br />
                 <br />
-                My mission is to be that support system—whether it&apos;s through tutoring, mentorship, or simply a listening ear. Together, we can break down barriers and show that our community&apos;s potential is limitless. Because when we lift each other up, we all rise higher.
+                My mission is to be that support system—whether it&apos;s through tutoring, mentorship, or simply listening. Together, we can break down barriers and show that potential is limitless when we support each other. When we lift each other up, we all rise higher.
             </p>
         </section>
     )
--- src/app/components/Unite.tsx
+++ src/app/components/Unite.tsx
@@ -2,15 +2,15 @@ export default function Unite() {
     return (
         <section id="unite" className="h-fit max-w-[1450px] mx-auto grid gap-10 bg-[#B3A369] text-[#121212] p-6 rounded-xl text-center">
             <div>
-                <h1 className="font-bold text-3xl md:text-5xl mb-2">Unite: Lifting Our Community Together</h1>
-                <p>Our community is rich with talent and potential, and by uniting, we can achieve amazing things. Whether you&apos;re a student seeking guidance, a parent looking for support, or someone eager to contribute, let&apos;s join forces.</p>
+                <h1 className="font-bold text-3xl md:text-5xl mb-2">Lending a Hand</h1>
+                <p>There are so many young people out there with talent and potential, and I&apos;m committed to helping them realize it. Whether you&apos;re a student seeking guidance, a parent looking for support, or someone eager to contribute, let&apos;s connect.</p>
             </div>
             <div>
-                <h2 className="text-2xl md:text-4xl font-bold">How We Can Unite</h2>
+                <h2 className="text-2xl md:text-4xl font-bold">How We Can Connect:</h2>
                 <ul className="mt-2 text-left">
                     <li className="py-2"><span className="font-bold">Need a Hand?</span> If you&apos;re feeling stuck with school or uncertain about the future, I&apos;m here to help turn those challenges into opportunities.</li>
-                    <li className="py-2"><span className="font-bold">Got Ideas?</span> If you&apos;re passionate about uplifting our community, let&apos;s collaborate. Whether it&apos;s a project, workshop, or something new, I&apos;m ready to work with you.</li>
-                    <li className="py-2"><span className="font-bold">Join the Fun!</span> It takes a village, and every bit of help counts. Whether you&apos;re volunteering, sharing resources, or spreading the word, your involvement makes a difference. Let&apos;s unite and create a thriving future together.</li>
+                    <li className="py-2"><span className="font-bold">Got Ideas?</span> If you&apos;re passionate about helping others, let&apos;s collaborate. Whether it&apos;s a project, workshop, or something new, I&apos;m ready to work with you.</li>
+                    <li className="py-2"><span className="font-bold">Join the Effort!</span>Your involvement matters. Whether you want to volunteer, share resources, or spread the word, every bit helps. Let&apos;s work together to make a difference.</li>
                 </ul>
             </div>
         </section>
