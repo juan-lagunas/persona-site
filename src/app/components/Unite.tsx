e5966c4e771c58ea74772850c0fa83551e8072cd
Juan Lagunas <juanlagunas28@gmail.com>
2024-08-27 12:08:05 -0400

    add new section

--- src/app/components/Unite.tsx
+++ src/app/components/Unite.tsx
@@ -1,18 +1,28 @@
 export default function Unite() {
     return (
-        <section id="empower" className="h-fit max-w-[1450px] mx-auto">
-            <h1 className="font-bold text-3xl md:text-5xl mb-2">Unite: Lifting Our Community Together</h1>
+        <section id="unite" className="h-fit max-w-[1450px] mx-auto grid gap-10">
             <div className="text-base md:text-lg">
-                Our Mexican community is brimming with talent and potential, and when we unite, we can achieve amazing things. Whether you&apos;re a student looking for guidance, a parent seeking support, or someone who simply wants to contribute, I invite you to join me on this journey.
-                <br />
-                <br />
-                <h2 className="text-xl md:text-3xl font-bold">How We Can Unite:</h2>
-                My mission is to be that support system—whether it&apos;s through tutoring, mentorship, or simply a listening ear. Together, we can break down barriers and show that our community&apos;s potential is limitless. Because when we lift each other up, we all rise higher.
-                <ul>
-                    <li>Need a Hand? If you&apos;re a student who&apos;s feeling stuck with schoolwork or unsure about the future, don&apos;t worry—I&apos;ve been there. Let&apos;s work together to turn those challenges into stepping stones.</li>
-                    <li>Got Ideas? If you&apos;re passionate about making a difference and have ideas on how we can uplift our community, let&apos;s unite our efforts. Whether it&apos;s a project, a workshop, or something brand new, I&apos;m ready to roll up my sleeves and get to work with you.</li>
-                    <li>Join the Fun! It takes a village, and every bit of help counts. Whether you&apos;re up for volunteering, sharing resources, or just spreading the word, your involvement makes a real impact. Let&apos;s unite and create a future where we all thrive.</li>
-                </ul>
+                <h1 className="font-bold text-3xl md:text-5xl mb-2">Unite: Lifting Our Community Together</h1>
+                <p>Our community is rich with talent and potential, and by uniting, we can achieve amazing things. Whether you&apos;re a student seeking guidance, a parent looking for support, or someone eager to contribute, let&apos;s join forces.</p>
+            </div>
+            <div className="bg-[#B3A369] text-[#121212] p-6 grid grid-cols-2 rounded-xl text-lg">
+                <div>
+                    <h2 className="text-2xl md:text-4xl font-bold text-center">How We Can Unite</h2>
+                    <ul className="mt-2">
+                        <li className="py-2"><span className="font-bold">Need a Hand?</span> If you&apos;re feeling stuck with school or uncertain about the future, I&apos;m here to help turn those challenges into opportunities.</li>
+                        <li className="py-2"><span className="font-bold">Got Ideas?</span> If you&apos;re passionate about uplifting our community, let&apos;s collaborate. Whether it&apos;s a project, workshop, or something new, I&apos;m ready to work with you.</li>
+                        <li className="py-2"><span className="font-bold">Join the Fun!</span> It takes a village, and every bit of help counts. Whether you&apos;re volunteering, sharing resources, or spreading the word, your involvement makes a difference. Let&apos;s unite and create a thriving future together.</li>
+                    </ul>
+                </div>
+                <div>
+                    <h2 className="text-2xl md:text-4xl font-bold text-center">Let’s Connect</h2>
+                    <ul className="h-[90%] flex flex-col mx-auto justify-center p-10 text-xl">
+                        <li><span className="font-bold">Email: </span>juanlagunas28@gmail.com</li>
+                        <li><span className="font-bold">Discord: </span>dajuanring</li>
+                        <li><span className="font-bold">Youtube: </span>https://www.youtube.com/@betillo-lagunas</li>
+                    </ul>
+                
+                </div>
             </div>
         </section>
     )
