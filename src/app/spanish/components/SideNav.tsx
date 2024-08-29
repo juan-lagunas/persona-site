9d67fcc6ead6be42adb3d1bfd79bb0b8b2983065
Juan Lagunas <juanlagunas28@gmail.com>
2024-08-29 10:57:59 -0400

    fix sidenav

--- src/app/spanish/components/SideNav.tsx
+++ src/app/spanish/components/SideNav.tsx
@@ -11,18 +11,18 @@ const SideNav: React.FC<SideNavProps> = ({ children }) => {
     return (
         <div className="flex flex-col h-dvh text-[#B3A369]">
             <div className="flex overflow-auto h-full">
-                <div className="grid grid-rows-3 w-[20%] max-w-[200px] h-full py-10 bg-[#0D0D0D]">
+                <div className="grid grid-rows-3 w-[22.5%] max-w-[200px] h-full py-10 bg-[#0D0D0D]">
                     <a href="#" className="text-center text-3xl font-bold">Tillo</a>
                     <div className="flex flex-col gap-y-6 items-center justify-center capitalize">
-                        <a href="#journey">Propósito</a>
-                        <a href="#empower">Empoderamiento</a>
-                        <a href="#connect">Ayuda</a>
+                        <a href="#journey">Misión</a>
+                        <a href="#empower">Acción</a>
+                        <a href="#connect">Unir</a>
                     </div>
                     <div className="flex flex-col items-center justify-end gap-y-4">
                         <a href="https://github.com/juan-lagunas" className="w-8 h-8 rounded-full flex items-center justify-center"><FontAwesomeIcon className="size-6" icon={faGithub} /></a>
                         <a href="https://www.youtube.com/@betillo-lagunas" className="w-8 h-8 rounded-full flex items-center justify-center"><FontAwesomeIcon className="size-6" icon={faYoutube} /></a>
                         <a href="mailto:juanlagunas28@gmail.com" className="w-8 h-8 rounded-full flex items-center justify-center"><FontAwesomeIcon className="size-6" icon={faEnvelope} /></a>
-                        <Link href="/english" className="bg-[#B3A369] text-[#0D0D0D] py-2 px-4 rounded-3xl opacity-90 hover:opacity-100 transition ease-in-out">English</Link>
+                        <Link href="/english" className="md:bg-[#B3A369] md:text-[#0D0D0D] md:py-2 md:px-4 md:rounded-3xl md:opacity-90 md:hover:opacity-100 md:transition md:ease-in-out">English</Link>
                     </div>
                 </div>
                 <main className="flex-1 overflow-y-auto bg-[#121212]">{children}</main>
