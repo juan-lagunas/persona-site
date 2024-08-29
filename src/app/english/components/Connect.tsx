34855421e1ae1a0f0a4e0b0a54504d7c85b393cc
Juan Lagunas <juanlagunas28@gmail.com>
2024-08-29 11:16:47 -0400

    change conenct section

--- src/app/english/components/Connect.tsx
+++ src/app/english/components/Connect.tsx
@@ -1,18 +1,13 @@
 export default function Unite() {
     return (
         <section id="connect" className="h-fit max-w-[1450px] mx-auto grid gap-10 bg-[#B3A369] text-[#121212] p-6 rounded-xl text-center">
-            <div>
-                <h1 className="font-bold text-3xl md:text-5xl mb-2">Lending a Hand</h1>
-                <p>There are so many young people out there with talent and potential, and I&apos;m committed to helping them realize it. Whether you&apos;re a student seeking guidance, a parent looking for support, or someone eager to contribute, let&apos;s connect.</p>
-            </div>
-            <div>
-                <h2 className="text-2xl md:text-4xl font-bold">How We Can Connect:</h2>
-                <ul className="mt-2 text-left">
-                    <li className="py-2"><span className="font-bold">Need a Hand?</span> If you&apos;re feeling stuck with school or uncertain about the future, I&apos;m here to help turn those challenges into opportunities.</li>
-                    <li className="py-2"><span className="font-bold">Got Ideas?</span> If you&apos;re passionate about helping others, let&apos;s collaborate. Whether it&apos;s a project, workshop, or something new, I&apos;m ready to get to work.</li>
-                    <li className="py-2"><span className="font-bold">Join the Effort!</span> Your involvement matters. Whether you want to volunteer, share resources, or spread the word, every bit helps. Let&apos;s work together to make a difference.</li>
-                </ul>
-            </div>
+            <h1 className="font-bold text-3xl md:text-5xl mb-2">Lending a Hand</h1>
+            <p>There are so many young people out there with talent and potential, and I&apos;m committed to helping them realize it. Whether you&apos;re a student seeking guidance, a parent looking for support, or someone eager to contribute, let&apos;s connect.</p>
+            <ul className="mt-2">
+                <li className="py-2"><span className="font-bold">Email: </span>juanlagunas28@gmail.com</li>
+                <li className="py-2"><span className="font-bold">Phone: </span>(404) 984-4174</li>
+                <li className="py-2"><span className="font-bold">WhatsApp: </span>(404) 984-4174</li>
+            </ul>
         </section>
     )
 }
--- src/app/spanish/components/Connect.tsx
+++ src/app/spanish/components/Connect.tsx
@@ -3,10 +3,10 @@ export default function Unite() {
         <section id="connect" className="h-fit max-w-[1450px] mx-auto grid gap-10 bg-[#B3A369] text-[#121212] p-6 rounded-xl text-center">
             <h1 className="font-bold text-3xl md:text-5xl mb-2">Echando una Mano</h1>
             <p>Hay tantos jóvenes en mi comunidad con talento y potencial, y estoy comprometido a ayudarlos a realizarlo. Ya sea que seas un estudiante buscando orientación, un padre en busca de apoyo, o alguien con ganas de contribuir, conectemos.</p>
-            <ul className="mt-2 text-left">
-                <li className="py-2"><span className="font-bold">¿Necesitas una Mano? </span> Si te sientes atascado en la escuela o no estás seguro sobre el futuro, estoy aquí para ayudarte a convertir esos desafíos en oportunidades.</li>
-                <li className="py-2"><span className="font-bold">¿Tienes Ideas?</span> Si tienes pasión por ayudar a los demás, colaboremos. Ya sea un proyecto, taller o algo nuevo, estoy listo para poner manos a la obra.</li>
-                <li className="py-2"><span className="font-bold">¡Únete al Esfuerzo!</span> Tu participación importa. Ya sea que quieras ser voluntario, compartir recursos o correr la voz, cada contribución cuenta. Trabajemos juntos para marcar la diferencia.</li>
+            <ul className="mt-2">
+                <li className="py-2"><span className="font-bold">Correo Electrónico: </span>juanlagunas28@gmail.com</li>
+                <li className="py-2"><span className="font-bold">Teléfono: </span>(404) 984-4174</li>
+                <li className="py-2"><span className="font-bold">WhatsApp: </span>(404) 984-4174</li>
             </ul>
         </section>
     )
